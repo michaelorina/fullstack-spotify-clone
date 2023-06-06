3def74490a024620160210f2a6076afd93c5c7e8
maestro <maestro@localhost.localdomain>
2023-06-06 12:02:02 +0300

    create functions and define classnames

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import React from "react";
+import { twMerge } from "tailwind-merge";
 
 interface HeaderProps{
     children: React.ReactNode;
@@ -8,9 +12,34 @@ const Header: React.FC<HeaderProps> = ({
     children,
     className
 }) =>{
+
+    const router = useRouter();
+
+    const handleLogout = () =>{
+        //handle logout in the future
+    }
     return(
-        <div>
-            Hello Header!
+        <div className={twMerge(`
+            h-fit
+            bg-gradient-to-b
+            from-emerald-800
+            p-6
+        `, className
+        )}>
+            <div className="
+                w-full
+                mb-4
+                flex
+                items-center
+                justify-between
+            ">
+                <div className="
+                    hidden
+                    md:flex
+                    gap-x-2
+                    items-center
+                "></div>
+            </div>
         </div>
     );
 }
