a2f820525207489ee0610075711d2cf9d2067fc3
michaelorina <orinamichael4@gmail.com>
2023-08-30 22:42:47 +0300

    FaPlay

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import React from "react";
 import Image from "next/image";
+import {FaPlay} from "react-icons/fa"
 
 interface ListItemProps {
     image: string;
@@ -22,7 +23,9 @@ const ListItem: React.FC<ListItemProps> = ({
         router.push(href);
     }
     return (
-        <button className="
+        <button 
+        onClick={onClick}
+        className="
             relative
             group
             flex
@@ -50,8 +53,22 @@ const ListItem: React.FC<ListItemProps> = ({
             <p className="font-medium truncate py-5">
                 {name}
             </p>
-            <div>
-                
+            <div className="
+                absolute
+                transition
+                opacity-0
+                rounded-full
+                flex
+                items-center
+                justify-center
+                bg-green-500
+                p-4
+                drop-shadow-md
+                right-5
+                group-hover:opacity-100
+                hover:scale-110
+            ">
+               <FaPlay className="text-black"/> 
             </div>
         </button>
     );
